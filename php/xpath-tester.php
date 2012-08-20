9149ffb2bc6bb93e71b59e27c46d55a717f58942
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2012-08-20 08:56:17 +0200

    Colorize result blocks

--- php/xpath-tester.php
+++ php/xpath-tester.php
@@ -1,5 +1,7 @@
 <?php
 
+    const HIGHLIGHT_TOKEN = 'XPATH-TESTER-HIGHLIGHT';
+
     // Trim is important here to avoid some empty lines at the beginning of the copy/paste.
     $xpathExpression = trim($_POST['xpath_expression']);
     $xmlInput = trim($_POST['xml_input']);
@@ -14,24 +16,43 @@
     $domDocument = new DOMDocument();
     $domDocument->loadXml($xmlInput);
 
-    $domDocument->formatOutput = true;
-    $domDocument->preserveWhiteSpace = true;
-
     $xpath = new DOMXPath($domDocument);
     $results = $xpath->query($xpathExpression);
 
     // Highlight matching elements by adding a correct span
-    $output = htmlentities($domDocument->saveXML(), ENT_QUOTES, 'utf-8');
+    $output = $domDocument->saveXML();
     
     $numberResults = $results->length;
     for($i = 0 ; $i < $numberResults ; $i++) {
         
-        // Retrieve match XML
+        // Retrieve matched XML
         $result = $results->item($i);
-        $result = htmlentities($result->ownerDocument->saveXML($result), ENT_QUOTES, 'utf-8');
+        $result = $result->ownerDocument->saveXML($result);
         
         // Highlight it
-        $output = str_replace($result, '<span class="highlight">'.$result.'</span>', $output);
+        $replacingString = sprintf('<!-- %s -->%s<!-- END-%s -->', HIGHLIGHT_TOKEN, $result, HIGHLIGHT_TOKEN);
+        $output = str_replace($result, $replacingString, $output);
+
     }
 
+    // Format the XML result
+    $domDocument = new DOMDocument();
+    $domDocument->loadXml($output);
+
+    // With some indentation
+    $domDocument->formatOutput = true;
+    $domDocument->preserveWhiteSpace = true;
+
+    // Convert all angle brackets to a correct display
+    $output = htmlentities($domDocument->saveXml(), ENT_QUOTES, 'utf-8');
+
+    // Replace highlighting comments by a highlighting span
+    $output = str_replace('&lt;!-- '.HIGHLIGHT_TOKEN.' --&gt;', '<span class="highlight">', $output);
+    $output = str_replace('&lt;!-- END-'.HIGHLIGHT_TOKEN.' --&gt;', '</span>', $output);
+
+    // Finally remove breaklines before and after highlighted blocks.
+    $output = preg_replace('#'.PHP_EOL.'( *)<span class="highlight">#', '<span class="highlight">', $output);
+    $output = preg_replace('#( *)</span>( *)'.PHP_EOL.'#', '</span>', $output);
+
     echo $output;
+
