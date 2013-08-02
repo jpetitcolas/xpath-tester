6ad267548b0ccf44331ec4692d912a8dc39b275b
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2013-08-02 06:55:40 +0200

    Replace error alert message by a less-aggressive div

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,7 +1,14 @@
 $(document).ready(function($) {
+
+	"use strict";
+
+	var errorContainer = $('#error');
+
 	$("#xpath_form").submit(function(e) {
 		e.preventDefault();
 		var that = this;
+
+		errorContainer.hide();
 
 		var request = $.ajax({
 			url: that.action,
@@ -12,7 +19,18 @@ $(document).ready(function($) {
 			$("#result").html(data.output);
 			$('#result-count').text(data.numberResults + " result(s) found");
 		}).fail(function(jqXHR, textStatus)	{
-			alert("Request failed: " + textStatus);
+			var message = "<strong>Oh snap!</strong> ";
+
+			switch(textStatus) {
+				case "parsererror":
+					message += "We were unable to parse your XML. Are you sure of its syntax?";
+					break;
+
+				default:
+					message += "An error occured, and the only clue is: " + textStatus + ".";
+			}
+
+			errorContainer.html(message).show();
 		});
 	});
 });
